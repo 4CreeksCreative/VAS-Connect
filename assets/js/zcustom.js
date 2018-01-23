01aa0ae8ba74cb1571b48cfed719d1e47f99b9ca
deepfry <jj@4cc.co>
2018-01-23 09:35:24 -0800

    fixed jquery animations with a .stop() in front of it

--- assets/js/zcustom.js
+++ assets/js/zcustom.js
@@ -9,21 +9,21 @@ $(document).on('scroll', function(){
 $(document).on('click', '#events .event .arrow', function(){
 	var event = $(this).parents('.event')
 	if (event.hasClass('active')){
-		event.removeClass('active').find('.event-description').slideToggle()
+		event.removeClass('active').find('.event-description').stop().slideToggle()
 	}
 	else {
-		event.addClass('active').find('.event-description').slideToggle()
+		event.addClass('active').find('.event-description').stop().slideToggle()
 	}
 })
 $(document).on('click', '#day2-show', function(){
 	$("#day2").find('.event').toggleClass('active')
-	$("#day2").find('.event-description').slideToggle()
+	$("#day2").find('.event-description').stop().slideToggle()
 })
 $(document).on('click', '#day1-show', function(){
 	$('#day1').find('.event').toggleClass('active')
-	$("#day1").find('.event-description').slideToggle()
+	$("#day1").find('.event-description').stop().slideToggle()
 })
 $(document).on('click', '#day3-show', function(){
 	$('#day3').find('.event').toggleClass('active')
-	$("#day3").find('.event-description').slideToggle()
+	$("#day3").find('.event-description').stop().slideToggle()
 })
